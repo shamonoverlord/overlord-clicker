8654c2c543063e4e59604c9d9e2aac6fd22f1ecc
shamonoverlord <shamon.overlord@gmail.com>
2026-06-22 20:00:38 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log("Game Start");
+let enemyMaxHp = 100;
+let enemyHp = 100;
+
+let gold = 0;
+
+const enemyArea = document.getElementById("enemy-area");
+const hpBar = document.getElementById("enemy-hp-bar");
+const hpText = document.getElementById("enemy-hp-text");
+const goldText = document.getElementById("gold");
+
+function updateEnemyUI() {
+
+    const percent = (enemyHp / enemyMaxHp) * 100;
+
+    hpBar.style.width = percent + "%";
+
+    hpText.textContent =
+        enemyHp + " / " + enemyMaxHp;
+}
+
+enemyArea.addEventListener("click", () => {
+
+    enemyHp -= 10;
+
+    if(enemyHp <= 0){
+
+        gold += 5;
+
+        goldText.textContent = gold;
+
+        enemyHp = enemyMaxHp;
+    }
+
+    updateEnemyUI();
+
+});
+
+updateEnemyUI();
