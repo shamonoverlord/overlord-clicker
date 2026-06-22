6686b4a9e6a9e21b095bd51294133137d4f625e6
shamonoverlord <shamon.overlord@gmail.com>
2026-06-22 21:56:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -53,7 +53,13 @@ document.addEventListener("contextmenu", (event) => {
     event.preventDefault();
 });
 
+document.addEventListener("selectstart", (event) => {
+    event.preventDefault();
+});
 
+document.addEventListener("dragstart", (event) => {
+    event.preventDefault();
+});
 
 
 
