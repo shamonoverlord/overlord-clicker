c79b9f1a896d01ecc94e4f6bd1d71a67aef63ea0
shamonoverlord <shamon.overlord@gmail.com>
2026-06-22 21:41:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,27 +3,40 @@ let enemyHp = 100;
 
 let gold = 0;
 
+const tapDamage = 10;
+
 const enemyArea = document.getElementById("enemy-area");
 const hpBar = document.getElementById("enemy-hp-bar");
 const hpText = document.getElementById("enemy-hp-text");
 const goldText = document.getElementById("gold");
 
 function updateEnemyUI() {
-
     const percent = (enemyHp / enemyMaxHp) * 100;
 
     hpBar.style.width = percent + "%";
 
-    hpText.textContent =
-        enemyHp + " / " + enemyMaxHp;
+    hpText.textContent = enemyHp + " / " + enemyMaxHp;
+}
+
+function showDamageText(damage) {
+    const damageText = document.createElement("div");
+
+    damageText.className = "damage-text";
+    damageText.textContent = damage;
+
+    enemyArea.appendChild(damageText);
+
+    damageText.addEventListener("animationend", () => {
+        damageText.remove();
+    });
 }
 
 enemyArea.addEventListener("click", () => {
+    enemyHp -= tapDamage;
 
-    enemyHp -= 10;
+    showDamageText(tapDamage);
 
-    if(enemyHp <= 0){
-
+    if (enemyHp <= 0) {
         gold += 5;
 
         goldText.textContent = gold;
@@ -32,7 +45,6 @@ enemyArea.addEventListener("click", () => {
     }
 
     updateEnemyUI();
-
 });
 
 updateEnemyUI();
